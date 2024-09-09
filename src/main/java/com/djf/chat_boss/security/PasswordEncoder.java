b76a85d2f681847ff87509e4bf0e23e840f59ee2
dj <darrellbrand@gmail.com>
2024-09-09 15:23:36 -0700

    initial

--- src/main/java/com/djf/chat_boss/security/PasswordEncoder.java
+++ src/main/java/com/djf/chat_boss/security/PasswordEncoder.java
@@ -2,14 +2,13 @@ package com.djf.chat_boss.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PasswordEncoderLocal
+public class PasswordEncoder
 {
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
