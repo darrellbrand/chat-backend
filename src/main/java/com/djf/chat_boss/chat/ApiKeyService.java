bc9f37228f913b839d6c072732565b0b822f32fb
dj <darrellbrand@gmail.com>
2024-09-11 03:52:48 -0700

    wip chat integration

--- src/main/java/com/djf/chat_boss/chat/ApiKeyService.java
+++ src/main/java/com/djf/chat_boss/chat/ApiKeyService.java
@@ -1,9 +1,13 @@
 package com.djf.chat_boss.chat;
 
+import io.getstream.chat.java.models.Channel;
 import io.getstream.chat.java.models.User;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ApiKeyService {
@@ -11,14 +15,48 @@ public class ApiKeyService {
     private String apiKey;
     @Value("${io.getstream.chat.apiSecret}")
     private String apiSecret;
-  @PostConstruct
-  public void init() {
-      System.out.println("apiKey: " + apiKey);
-      System.out.println("apiSecret: " + apiSecret);
-      System.setProperty("io.getstream.chat.apiKey", apiKey);
-      System.setProperty("io.getstream.chat.apiSecret", apiSecret);
-  }
-    public String getToken(String name){
-        return User.createToken(name,null,null);
+
+    @PostConstruct
+    public void init() {
+        System.out.println("apiKey: " + apiKey);
+        System.out.println("apiSecret: " + apiSecret);
+        System.setProperty("io.getstream.chat.apiKey", apiKey);
+        System.setProperty("io.getstream.chat.apiSecret", apiSecret);
+
+
+    }
+
+    public String getToken(String name) {
+
+        String token = null;
+        try {
+            var AdminUser =
+                    User.UserRequestObject.builder()
+                            .name("BACKEND-ADMIN")
+                            .id("BACKEND-ADMIN")
+                            .build();
+
+            Channel.getOrCreate("messaging", "my-team-channel")
+                    .data(
+                            Channel.ChannelRequestObject.builder()
+                                    .createdBy(AdminUser)
+                                    .build())
+                    .request();
+            Channel.update("messaging", "my-team-channel")
+                    .data(Channel.ChannelRequestObject.builder()
+                            .additionalField("name", "my-team-channel")
+                            .additionalField("color", "green")
+                            .build())
+                    .request();
+            token = User.createToken(name, null, null);
+            var user = User.UserRequestObject.builder().id(name).role("admin").build();
+            User.upsert().user(user).request();
+            Channel.update("messaging", "my-team-channel").addMember(name).hideHistory(true).request();
+            // Channel.update("messaging", "my-team-channel").removeMember(name).hideHistory(true).request();
+            //  Channel.delete("messaging", "my-team-channel");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return token;
     }
 }
